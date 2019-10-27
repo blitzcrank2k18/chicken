cd59c75fdb06477c677bd9f2ada11c5618143332
Honeylee Magbanua <emoblazz@gmail.com>
2019-10-27 19:26:10 +0800

    print button and delete undefined

--- recording/live_weight_remove.php
+++ recording/live_weight_remove.php
@@ -6,7 +6,7 @@ endif;
 include('../dist/includes/dbcon.php');
 	$id = $_POST['id'];
 	$lwid = $_POST['lwid'];
-	$weight =$_POST['weight'];
+	//$weight =$_POST['weight'];
 
 	$result=mysqli_query($con,"DELETE FROM live_weight WHERE live_weight_id ='$lwid'")
 	or die(mysqli_error());
